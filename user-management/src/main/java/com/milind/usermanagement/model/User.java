ea927ae82428008643cb40979f24e1ca41087835
MilindVedi <100509743+MilindVedi@users.noreply.github.com>
2023-05-01 17:12:34 +0530

    Add files via upload

---
+++ user-management/src/main/java/com/milind/usermanagement/model/User.java
@@ -0,0 +1,53 @@
+package com.milind.usermanagement.model;
+
+public class User {
+    private Integer userId;
+
+    private String name;
+
+    private String username;
+
+    private String address;
+
+    private String phoneNo;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+}
